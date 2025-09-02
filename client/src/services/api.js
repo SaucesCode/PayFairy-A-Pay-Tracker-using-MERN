cb314256883025efed254756523de60ee25e026d
James Patrick De Mesa <83571819+SaucesCode@users.noreply.github.com>
2025-09-03 02:30:43 +0800

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// Use environment variable if available, else fallback to dev/prod defaults
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  (import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "/api");
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
+export { BASE_URL }; // so you can use this for images
 export default api;
